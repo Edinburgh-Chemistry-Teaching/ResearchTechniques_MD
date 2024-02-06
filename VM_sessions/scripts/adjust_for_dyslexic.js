081f6afb01eb8f4e77694b8638f977835dc58df3
s2123329 <h.pollak@sms.ed.ac.uk>
2024-02-06 14:08:47 +0000

    Add dyslexia-friendly style

--- VM_sessions/scripts/adjust_for_dyslexic.js
+++ VM_sessions/scripts/adjust_for_dyslexic.js
@@ -7,37 +7,58 @@ function adjustStyle() {
     for (let i = 0; i < linkElements.length; i++) {
         if (linkElements[i].getAttribute('href').endsWith('scripts/style.css')) {
             linkElements[i].setAttribute('href', '../scripts/style_dyslexia.css');
-            // let newButton = button.cloneNode(true);
-            button.innerHTML = 'Regular display';
-            button.className = 'dyslexiaButton'
-            // button.parentNode.replaceChild(newButton, button);
+            let newButton = button.cloneNode(true);
+            newButton.innerHTML = 'Regular display';
+            newButton.className = 'dyslexiaButton'
+            // button.innerHTML = 'Regular display';
+            // button.className = 'dyslexiaButton'
+            newButton.addEventListener('click', function() {
+                console.log('Button clicked')
+                adjustStyle();
+            });
+            button.parentNode.replaceChild(newButton, button);
         } else if ((linkElements[i].getAttribute('href').endsWith('scripts/style_dyslexia.css'))) {
             linkElements[i].setAttribute('href', '../scripts/style.css');
-            // let newButton = button.cloneNode(true);
-            button.innerHTML = 'dyslexia-friendly display'
-            button.className = 'dyslexiaButton'
+            let newButton = button.cloneNode(true);
+            newButton.innerHTML = 'Dyslexia-friendly display';
+            newButton.className = 'dyslexiaButton'
+            newButton.addEventListener('click', function() {
+                console.log('Button clicked')
+                adjustStyle();
+            });
+            button.parentNode.replaceChild(newButton, button);
+            // button.innerHTML = 'dyslexia-friendly display'
+            // button.className = 'dyslexiaButton'
             // button.parentNode.replaceChild(newButton, button);
         }
     }
 }
-
 function adjustForDyslexia() {
     let button = document.createElement('button');
     button.className = 'dyslexiaButton';
-    button.id = 'dyslexiaButton';
-    button.innerHTML = 'Dyslexia-friendly display';
-    button.style.display = 'block';
-    button.style.position = 'fixed';
-    button.style.right = '20px';
-    button.style.top = '20px';
     document.body.appendChild(button);
-    // button.addEventListener('click', function() {
-    //     console.log('Button clicked')
-    //     adjustStyle();
-    // });
-    button.addEventListener('click', adjustStyle)
+    button.addEventListener('click', function() {
+        console.log('Button clicked')
+        adjustStyle();
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    adjustForDyslexia();
-});
+
+// function adjustForDyslexia() {
+//     let button = document.createElement('button');
+//     button.className = 'dyslexiaButton';
+//     button.id = 'dyslexiaButton';
+//     button.innerHTML = 'Dyslexia-friendly display';
+//     // button.style.display = 'block';
+//     button.style.position = 'fixed';
+//     // button.style.right = '20px';
+//     // button.style.top = '20px';
+//     document.body.appendChild(button);
+//     // button.addEventListener('click', function() {
+//     //     console.log('Button clicked')
+//     //     adjustStyle();
+//     // });
+
+// }
+
+
