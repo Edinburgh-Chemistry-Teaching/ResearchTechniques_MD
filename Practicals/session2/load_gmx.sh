dcfffc7f52050a92df4db110f25065f46b3739de
Sarah Stewart <sarahvs448@gmail.com>
2024-12-20 16:57:38 +0000

    Updating session 1

--- Practicals/session2/load_gmx.sh
+++ Practicals/session2/load_gmx.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 . /etc/profile.d/modules.sh
-module load /exports/applications/modulefiles/Community/igmm/apps/cuda/12.1.1
-module load /exports/applications/modulefiles/Community/phys/compilers/gcc/11.2.0
-source /exports/applications/apps/SL7/gromacs/2023.3/gromacs_final/bin/GMXRC
+module load gromacs/2024.4
--- Practicals/session3/load_gmx.sh
+++ Practicals/session3/load_gmx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 . /etc/profile.d/modules.sh
-module load cuda/12.1.1 phys/compilers/gcc/11.2.0
-source /exports/applications/apps/SL7/gromacs/2023.3/gromacs_final/bin/GMXRC
+module load gromacs/2024.4
