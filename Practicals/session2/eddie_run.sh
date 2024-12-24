b245f9f3c9dfe04e6698d657ff150e950c47096b
Sarah Stewart <sarahvs448@gmail.com>
2024-12-24 13:05:35 +0000

    Updating feedback links

--- Practicals/session2/eddie_run.sh
+++ Practicals/session2/eddie_run.sh
@@ -11,18 +11,16 @@
 # Request one slot on the gpu-a100 parallel environment
 #$ -pe gpu-a100 1
 
-# Request 16 GB system RAM
+# Request 512 GB system RAM
 # the total system RAM available to the job is the value specified here multiplied by  the number of requested GPUs (above)
-#$ -l h_vmem=8G
+#$ -l h_vmem=512G
 
 # Combine standard output and error files into a single job file with job ID
 #$ -j y
 
 # Initialise the environment modules and load CUDA and Gromacs
 . /etc/profile.d/modules.sh
-module load /exports/applications/modulefiles/Community/igmm/apps/cuda/12.1.1
-module load /exports/applications/modulefiles/Community/phys/compilers/gcc/11.2.0
-source /exports/applications/apps/SL7/gromacs/2023.3/gromacs_final/bin/GMXRC
+module load gromacs/2024.4
 
 # Add your commands here
 gmx grompp # add your options here
